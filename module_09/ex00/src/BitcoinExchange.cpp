c03b7f7d7eaf58863202d2577890f41c00179357
Tijmen De Bruine <tijmendebruine@Tijmens-MacBook-Pro.local>
2024-03-21 23:35:06 +0100

    Solving ex00 with STL map

--- module_09/ex00/src/BitcoinExchange.cpp
+++ module_09/ex00/src/BitcoinExchange.cpp
@@ -2,6 +2,7 @@
 
 BitcoinExchange::BitcoinExchange()
 {
+	first_line_skipped = false;
 }
 
 BitcoinExchange::~BitcoinExchange()
@@ -19,12 +20,56 @@ BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &bitcoinexchan
 	return *this;
 }
 
-void BitcoinExchange::Parseline(std::string line)
+void BitcoinExchange::printExchangeRates()
 {
-	std::string key;
-	double value;
-	std::stringstream ss(line);
-	ss >> key >> value;
-	_exchange_rates[key] = value;
+	for (auto &it : _exchange_rates)
+	{
+		std::cout << it.first << ": " << it.second << std::endl;
+	}
 }
 
+void BitcoinExchange::parseCvsLine(std::string line)
+{
+	std::istringstream iss(line);
+	std::string date, rate;
+	std::getline(iss, date, ',');
+	std::getline(iss, rate);
+	_exchange_rates[date] = std::stod(rate);
+}
+
+void BitcoinExchange::CompareDatabases(char *filename)
+{
+	std::ifstream file(filename);
+	if (!file.is_open())
+	{
+		std::cerr << "Error: could not open file" << std::endl;
+		exit(1);
+	}
+	std::string line;
+	while (std::getline(file, line))
+	{
+		std::istringstream iss(line);
+		std::string date, amount;
+		std::getline(iss, date, ' ');
+		
+	}
+}
+void BitcoinExchange::parseCvs()
+{
+	std::ifstream file("data.csv");
+	if (!file.is_open())
+	{
+		std::cerr << "Error: could not open file" << std::endl;
+		exit(1);
+	}
+	std::string line;
+	while (std::getline(file, line))
+	{
+		if (!first_line_skipped)
+		{
+			first_line_skipped = true;
+			continue;
+		}
+		parseCvsLine(line);
+	}
+}
