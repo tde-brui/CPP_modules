ee94a3abfaa97372ee9ff33726e8e15db805ffca
Tijmen De Bruine <tijmendebruinebusiness@gmail.com>
2024-09-06 13:33:40 +0200

    added edge case checking

--- module_06/ex00/src/display.cpp
+++ module_06/ex00/src/display.cpp
@@ -1,4 +1,5 @@
 #include "ScalarConverter.hpp"
+#include <limits>
 
 void display_char(const std::string &input)
 {
@@ -11,53 +12,102 @@ void display_char(const std::string &input)
 
 void display_int(const std::string &input)
 {
-	int i = std::stoi(input);
-	if (static_cast<char>(i) < 32 || static_cast<char>(i) > 126)
-		std::cout << "char: Non displayable" << std::endl;
-	else
-		std::cout << "char: " << static_cast<char>(i) << std::endl;
-	std::cout << "int: " << i << std::endl;
-	std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
-	std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
+	try
+	{
+		long long ll = std::stoll(input);
+		{
+		if (static_cast<int>(ll) < 32 || static_cast<int>(ll) > 126)
+			std::cout << "char: Non displayable" << std::endl;
+		else
+			std::cout << "char: " << static_cast<char>(ll) << std::endl;
+		if (ll > std::numeric_limits<int>::max() || ll < std::numeric_limits<int>::min())
+			std::cout << "int: overflow" << std::endl;
+		else
+			std::cout << "int: " << ll << std::endl;
+		if (ll > std::numeric_limits<float>::max() || ll < -std::numeric_limits<float>::max())
+			std::cout << "float: overflow" << std::endl;
+		else
+			std::cout << "float: " << static_cast<float>(ll) << ".0f" << std::endl;
+		if (ll > std::numeric_limits<double>::max() || ll < -std::numeric_limits<double>::max())
+			std::cout << "double: overflow" << std::endl;
+		else
+			std::cout << "double: " << static_cast<double>(ll) << ".0" << std::endl;
+		}
+	}
+	catch (std::out_of_range &e)
+	{
+		std::cout << "Error: int overflow" << std::endl;
+	}
+	catch (std::invalid_argument &e)
+	{
+		std::cout << "Error: invalid argument" << std::endl;
+	}
 }
 
 void display_float(const std::string &input)
 {
-	double f = std::stof(input);
-	if (static_cast<char>(f) < 32 || static_cast<char>(f) > 126)
-		std::cout << "char: Non displayable" << std::endl;
-	else
-		std::cout << "char: " << static_cast<char>(f) << std::endl;
-	std::cout << "int: " << static_cast<int>(f) << std::endl;
-	if (static_cast<int>(f) - f == 0)
-	{
-		std::cout << "float: " << f << ".0f" << std::endl;
-		std::cout << "double " << static_cast<double>(f) << ".0" << std::endl;
+	try{		
+		double f = std::stof(input);
+		if (static_cast<int>(f) < 32 || static_cast<int>(f) > 126)
+			std::cout << "char: Non displayable" << std::endl;
+		else
+			std::cout << "char: " << static_cast<char>(f) << std::endl;
+		if (f > std::numeric_limits<int>::max() || f < std::numeric_limits<int>::min())
+			std::cout << "int: overflow" << std::endl;
+		else
+			std::cout << "int: " << static_cast<int>(f) << std::endl;
+		if (static_cast<int>(f) - f == 0)
+		{
+			std::cout << "float: " << f << ".0f" << std::endl;
+			std::cout << "double " << static_cast<double>(f) << ".0" << std::endl;
+		}
+		else
+		{
+			std::cout << "float: " << f << "f" << std::endl;
+			std::cout << "double " << static_cast<double>(f) << std::endl;
+		}
 	}
-	else
+	catch (std::out_of_range &e)
 	{
-		std::cout << "float: " << f << "f" << std::endl;
-		std::cout << "double " << static_cast<double>(f) << std::endl;
+		std::cout << "Error: float overflow" << std::endl;
+	}
+	catch (std::invalid_argument &e)
+	{
+		std::cout << "Error: invalid argument" << std::endl;
 	}
 }
 
 void display_double(const std::string &input)
 {
-	double d = stod(input);
-	if (static_cast<char>(d) < 32 || static_cast<char>(d) > 126)
-		std::cout << "char: Non displayable" << std::endl;
-	else
-		std::cout << "char: " << static_cast<char>(d) << std::endl;
-	std::cout << "int: " << static_cast<int>(d) <<  std::endl;
-	if (static_cast<int>(d) - d == 0)
+	try
 	{
-		std::cout << "float: " << static_cast<float>(d)<< ".0f" << std::endl;
-		std::cout << "double: " << d << ".0" << std::endl;
+		double d = stod(input);
+		if (static_cast<int>(d) < 32 || static_cast<int>(d) > 126)
+			std::cout << "char: Non displayable" << std::endl;
+		else
+			std::cout << "char: " << static_cast<char>(d) << std::endl;
+		if (d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min())
+			std::cout << "int: overflow" << std::endl;
+		else
+			std::cout << "int: " << static_cast<int>(d) <<  std::endl;
+		if (static_cast<int>(d) - d == 0)
+		{
+			std::cout << "float: " << static_cast<float>(d)<< ".0f" << std::endl;
+			std::cout << "double: " << d << ".0" << std::endl;
+		}
+		else
+		{
+			std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
+			std::cout << "double: " << d << std::endl;
+		}
 	}
-	else
+	catch(std::out_of_range &e)
 	{
-		std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
-		std::cout << "double: " << d << std::endl;
+		std::cout << "Error: double overflow" << std::endl;
+	}
+	catch (std::invalid_argument &e)
+	{
+		std::cout << "Error: invalid argument" << std::endl;
 	}
 }
 
